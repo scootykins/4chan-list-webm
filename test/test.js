ad64e5d90ca98f93ea4d041f11becdf0d8725661
Philip Scott <pscott@zeptohost.com>
2018-01-28 23:30:31 -0800

    BREAKING CHANGE: Replace callback library with promise-based library

--- test/test.js
+++ test/test.js
@@ -8,6 +8,6 @@ describe('/GET webm data from a thread', function () {
       .then(res => res.data[0].threads[1].no)
       .then(threadNo => listWebms('wsg', threadNo))
       .then(data => expect(data).to.be.an('array').that.is.not.empty)
-      .catch(console.error)
+      .catch(err => coonsole.error('ERROR: ', err))
   })
 })
